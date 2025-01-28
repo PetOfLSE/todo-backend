43b97cf332233aa9d91993530abf61e7493c4126
unname <lwer210@naver.com>
2025-01-29 00:44:44 +0900

    feature: Refresh Token Entity Repository 추가

---
+++ src/main/java/com/example/todo/auth/persistence/repository/RefreshTokenEntityRepository.java
@@ -0,0 +1,13 @@
+package com.example.todo.auth.persistence.repository;
+
+import com.example.todo.auth.persistence.entity.RefreshTokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenEntityRepository extends JpaRepository<RefreshTokenEntity, Long> {
+
+    boolean existsByUserSeq(Long userSeq);
+
+    Optional<RefreshTokenEntity> findByUserSeq(Long userSeq);
+}
