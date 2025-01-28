b691404df368a0fd41e847626d896f4385a451e1
unname <lwer210@naver.com>
2025-01-28 18:20:15 +0900

    feature: swagger 설정 추가

---
+++ src/main/java/com/example/todo/common/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.todo.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(info())
+                .components(new Components());
+    }
+
+    public Info info(){
+        return new Info()
+                .description("todo list backend api")
+                .title("Todo List API Swagger")
+                .version("1.0");
+    }
+}
