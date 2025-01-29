e95ded5306c7691b7ebb82313db8652ba9f212c4
unname <lwer210@naver.com>
2025-01-29 18:38:54 +0900

    feature: 사용자 API 비즈니스 로직 추가

---
+++ src/main/java/com/example/todo/user/service/UserService.java
@@ -0,0 +1,46 @@
+package com.example.todo.user.service;
+
+import com.example.todo.auth.controller.response.UserResponse;
+import com.example.todo.auth.persistence.entity.UserEntity;
+import com.example.todo.auth.persistence.repository.UserEntityRepository;
+import com.example.todo.common.custom.CustomUserDetails;
+import com.example.todo.common.exception.UserNotFoundException;
+import com.example.todo.todos.controller.response.TodoResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+    private final UserEntityRepository userEntityRepository;
+
+    public UserResponse info(CustomUserDetails userDetails) {
+        UserEntity entity = userEntityRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없음"));
+
+        List<TodoResponse> todo = entity.getTodo().stream()
+                .map(value -> {
+                    return TodoResponse.builder()
+                            .id(value.getId())
+                            .content(value.getContent())
+                            .completed(value.isCompleted())
+                            .createdAt(value.getCreatedAt())
+                            .updatedAt(value.getUpdatedAt())
+                            .build();
+                })
+                .toList();
+
+        return UserResponse.builder()
+                .id(entity.getId())
+                .role(entity.getRole())
+                .email(entity.getEmail())
+                .nickname(entity.getNickname())
+                .todo(todo)
+                .build();
+    }
+}
