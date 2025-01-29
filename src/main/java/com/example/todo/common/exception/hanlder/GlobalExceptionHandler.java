fa1bd9b42ea476200efdf1fc13a9c01a643efbef
unname <lwer210@naver.com>
2025-01-29 18:07:10 +0900

    feature: 예외 핸들러 추가

--- src/main/java/com/example/todo/common/exception/hanlder/GlobalExceptionHandler.java
+++ src/main/java/com/example/todo/common/exception/hanlder/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.todo.common.exception.hanlder;
 
-import com.example.todo.common.exception.AlreadyEmailRegisterException;
-import com.example.todo.common.exception.AlreadyNicknameRegisterException;
+import com.example.todo.common.exception.*;
 import com.example.todo.common.exception.response.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,46 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = ExpiredTokenException.class)
+    public ResponseEntity<ExceptionResponse> handleExpiredJwtException(ExpiredTokenException e) {
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(e.getStatus().value())
+                .build();
+
+        return new ResponseEntity<>(response, e.getStatus());
+    }
+
+    @ExceptionHandler(value = HasNotRoleTokenException.class)
+    public ResponseEntity<ExceptionResponse> handleHasNotRoleTokenException(HasNotRoleTokenException e) {
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(e.getStatus().value())
+                .build();
+
+        return new ResponseEntity<>(response, e.getStatus());
+    }
+
+    @ExceptionHandler(value = TokenNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleTokenNotFoundException(TokenNotFoundException e) {
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(e.getStatus().value())
+                .build();
+
+        return new ResponseEntity<>(response, e.getStatus());
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleUserNotFoundException(UserNotFoundException e) {
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(e.getStatus().value())
+                .build();
+
+        return new ResponseEntity<>(response, e.getStatus());
+    }
 
     @ExceptionHandler(value = AlreadyEmailRegisterException.class)
     public ResponseEntity<ExceptionResponse> alreadyEmailRegisterException(AlreadyEmailRegisterException e) {
@@ -31,10 +70,10 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, e.getStatus());
     }
 
-    @ExceptionHandler(value = RuntimeException.class)
+    @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ExceptionResponse> handle(Exception e) {
         ExceptionResponse response = ExceptionResponse.builder()
-                .message("알 수 없는 예외 발생")
+                .message(e.getMessage())
                 .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .build();
 
