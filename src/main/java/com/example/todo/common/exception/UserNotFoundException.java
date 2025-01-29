68f7d320957f5ed9a0ae97f96a0379c810807f74
unname <lwer210@naver.com>
2025-01-29 18:05:40 +0900

    feature: 사용자를 찾지 못했을 때 던질 예외 추가

---
+++ src/main/java/com/example/todo/common/exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.example.todo.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class UserNotFoundException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public UserNotFoundException(String message) {
+        super(message);
+        this.status = HttpStatus.NOT_FOUND;
+    }
+}
