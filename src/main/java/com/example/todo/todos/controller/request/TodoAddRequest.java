925cabd73d413fab4aebc04f4a157bdef53995ba
unname <lwer210@naver.com>
2025-01-29 19:33:09 +0900

    feature: 할일 추가 요청 객체 추가

---
+++ src/main/java/com/example/todo/todos/controller/request/TodoAddRequest.java
@@ -0,0 +1,14 @@
+package com.example.todo.todos.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoAddRequest {
+
+    private String content;
+
+}
