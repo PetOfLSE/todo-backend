a5eb17005dee6d1b41fcb949f8694c1af52f3a2c
unname <lwer210@naver.com>
2025-01-29 19:32:34 +0900

    feature: swagger 명세 추가

--- src/main/java/com/example/todo/user/controller/UserController.java
+++ src/main/java/com/example/todo/user/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.example.todo.user.controller;
 
+import com.example.todo.auth.controller.response.UserResponse;
 import com.example.todo.common.custom.CustomUserDetails;
+import com.example.todo.common.exception.response.ExceptionResponse;
 import com.example.todo.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +22,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
-@Tag(name = "User API Controller")
+@Tag(name = "사용자 API Controller")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "사용자 조회 API", description = "사용자 개별 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))
+            }, description = "성공 시 반환"),
+            @ApiResponse(responseCode = "404", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))
+            }, description = "사용자를 찾을 수 없을 경우 반환"),
+            @ApiResponse(responseCode = "401", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))
+            }, description = "인증되지 않은 사용자일 경우 반환")
+    })
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/info")
     @SecurityRequirement(name = "bearerAuth")
